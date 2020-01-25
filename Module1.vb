43b9eefcf2d464903d7e9888f2bf2b4978b8d953
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2020-01-25 19:37:26 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Names As String
+        FileOpen(1, "C:\FilePractice\sRec.txt", OpenMode.Output)
+        For c = 1 To 5
+            Console.WriteLine("Enter student name: ")
+            Names = Console.ReadLine()
+            WriteLine(1, Names)
+        Next
+        FileClose(1)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
