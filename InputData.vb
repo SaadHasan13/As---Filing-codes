11ee1134e48ad3ef992955e57a2cd4073cadef7a
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2020-01-27 22:30:13 +0500

    May/June 2016 paper(2) variant(1) Q5(b)

---
+++ InputData.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Call InputData()
+        Console.ReadKey()
+    End Sub
+    Sub InputData()
+        Dim CDTitle, CDArtist, CDLocation As String
+        FileOpen(1, "C:\FilePractice\MyMusic.txt", OpenMode.Output)
+        Console.WriteLine("Input CD Title : ")
+        CDTitle = Console.ReadLine()
+        Do Until CDTitle = "##"
+            Console.WriteLine("Input CD Artist : ")
+            CDArtist = Console.ReadLine()
+            Console.WriteLine("Input CD Location : ")
+            CDLocation = Console.ReadLine()
+            Print(1, CDTitle & ":" & CDArtist & ":" & CDLocation)
+            Console.WriteLine("Input CD Title : ")
+            CDTitle = Console.ReadLine()
+        Loop
+        FileClose(1)
+    End Sub
+End Module
