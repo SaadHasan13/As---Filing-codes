755c976dce1178c5099fc1c7557609d5289550c3
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2020-01-30 22:04:44 +0500

    Task 1.3 of the assignment given

---
+++ AssignmentTask3.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberName, IdNumber, SearchName As String
+        Dim MemberFound As Boolean
+        MemberFound = False
+        Console.WriteLine("Enter name to search for : ")
+        SearchName = Console.ReadLine
+        FileOpen(1, "C:\FilePractice\ClubMembers.txt", OpenMode.Input)
+
+        While Not EOF(1) And MemberFound = False
+            Input(1, MemberName)
+            Input(1, IdNumber)
+
+            If MemberName = SearchName Then
+                MemberFound = True
+
+                Console.WriteLine("Id number :" & IdNumber)
+            End If
+        End While
+        FileClose(1)
+        If MemberFound = False Then Console.WriteLine("Record not found.")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
