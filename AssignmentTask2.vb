96dab6dbb9b296358079680ac9e4307e51edc678
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2020-01-30 21:21:41 +0500

    Task 1.2 of the assignment given

---
+++ AssignmentTask2.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberName, IDNumber As String
+        Dim C As Integer
+
+        FileOpen(1, "C:\FilePractice\ClubMembers.txt", OpenMode.Input)
+
+
+        For C = 1 To 5
+            Console.WriteLine("Enter a member's name :" & MemberName)
+            Console.WriteLine("Enter the member's Id number :" & IDNumber)
+            Input(1, MemberName)
+            Input(1, IDNumber)
+        Next
+
+        FileClose(1)
+        Console.ReadKey()
+    End Sub
+
+End Module
